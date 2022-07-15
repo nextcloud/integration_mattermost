e480b7788546a2c9c1c9571647aa9e19754d398a
Julien Veyssier <eneiluj@posteo.net>
2022-07-15 16:51:13 +0200

    add example to validate webhook signature
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ examples/receiveWebhook.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Example process to validate a webhook request signature
+ */
+
+$requestBody = file_get_contents('php://input');
+$secret = '123';
+
+// get the request signature
+$headers = getallheaders();
+$signature = $headers['X-Webhook-Signature'] ?? '';
+
+// compute the signature
+$bodyHash = hash('sha256', $requestBody . $secret);
+
+$content = $requestBody
+	. "\n\nReceived signature: " . $signature
+	. "\n\nSignature check:    " . $bodyHash;
+
+file_put_contents('/tmp/a', $content);
+?>
