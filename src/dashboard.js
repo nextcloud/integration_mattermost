132f2ba34b79992ba83522a884f6b1207dbcb094
Julien Veyssier <julien-nc@posteo.net>
2023-02-10 00:36:00 +0100

    update npm pkgs, lazy load dashboard widget
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,5 +1,3 @@
-/* jshint esversion: 6 */
-
 /**
  * Nextcloud - mattermost
  *
@@ -11,17 +9,17 @@
  * @copyright Julien Veyssier 2022
  */
 
-import Vue from 'vue'
-import './bootstrap.js'
-import Dashboard from './views/Dashboard.vue'
+__webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
+__webpack_public_path__ = OC.linkTo('integration_mattermost', 'js/') // eslint-disable-line
 
-document.addEventListener('DOMContentLoaded', function() {
-
-	OCA.Dashboard.register('mattermost_notifications', (el, { widget }) => {
+document.addEventListener('DOMContentLoaded', () => {
+	OCA.Dashboard.register('mattermost_notifications', async (el, { widget }) => {
+		const { default: Vue } = await import(/* webpackChunkName: "dashboard-lazy" */'vue')
+		Vue.mixin({ methods: { t, n } })
+		const { default: Dashboard } = await import(/* webpackChunkName: "dashboard-lazy" */'./views/Dashboard.vue')
 		const View = Vue.extend(Dashboard)
 		new View({
 			propsData: { title: widget.title },
 		}).$mount(el)
 	})
-
 })
