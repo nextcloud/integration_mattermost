4e67d63057720a809fdc78d54b98069a780dd861
Anupam Kumar <kyteinsky@gmail.com>
2024-01-15 18:26:10 +0530

    split off request method to NetworkService
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- lib/Service/MattermostAPIService.php
+++ lib/Service/MattermostAPIService.php
@@ -17,6 +17,7 @@ use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
 use OC\User\NoUserException;
 use OCA\Mattermost\AppInfo\Application;
+use OCA\Mattermost\Service\NetworkService;
 use OCP\Constants;
 use OCP\Files\File;
 use OCP\Files\Folder;
@@ -32,7 +33,6 @@ use OCP\PreConditionNotMetException;
 use OCP\Share\IManager as ShareManager;
 use OCP\Share\IShare;
 use Psr\Log\LoggerInterface;
-use Throwable;
 
 /**
  * Service to make requests to Mattermost API
@@ -42,13 +42,13 @@ class MattermostAPIService {
 	private IClient $client;
 
 	public function __construct(
-		string $appName,
 		private LoggerInterface $logger,
 		private IL10N $l10n,
 		private IConfig $config,
 		private IRootFolder $root,
 		private ShareManager $shareManager,
 		private IURLGenerator $urlGenerator,
+		private NetworkService $networkService,
 		IClientService $clientService
 	) {
 		$this->client = $clientService->newClient();
@@ -539,72 +539,11 @@ class MattermostAPIService {
 		string $endPoint,
 		array $params = [],
 		string $method = 'GET',
-		bool $jsonResponse = true
+		bool $jsonResponse = true,
 	) {
 		$mattermostUrl = $this->getMattermostUrl($userId);
 		$this->checkTokenExpiration($userId, $mattermostUrl);
-		$accessToken = $this->config->getUserValue($userId, Application::APP_ID, 'token');
-		try {
-			$url = $mattermostUrl . '/api/v4/' . $endPoint;
-			$options = [
-				'headers' => [
-					'Authorization' => 'Bearer ' . $accessToken,
-					'Content-Type' => 'application/x-www-form-urlencoded',
-					'User-Agent' => Application::INTEGRATION_USER_AGENT,
-				],
-			];
-
-			if (count($params) > 0) {
-				if ($method === 'GET') {
-					// manage array parameters
-					$paramsContent = '';
-					foreach ($params as $key => $value) {
-						if (is_array($value)) {
-							foreach ($value as $oneArrayValue) {
-								$paramsContent .= $key . '[]=' . urlencode($oneArrayValue) . '&';
-							}
-							unset($params[$key]);
-						}
-					}
-					$paramsContent .= http_build_query($params);
-
-					$url .= '?' . $paramsContent;
-				} else {
-					$options['json'] = $params;
-				}
-			}
-
-			if ($method === 'GET') {
-				$response = $this->client->get($url, $options);
-			} elseif ($method === 'POST') {
-				$response = $this->client->post($url, $options);
-			} elseif ($method === 'PUT') {
-				$response = $this->client->put($url, $options);
-			} elseif ($method === 'DELETE') {
-				$response = $this->client->delete($url, $options);
-			} else {
-				return ['error' => $this->l10n->t('Bad HTTP method')];
-			}
-			$body = $response->getBody();
-			$respCode = $response->getStatusCode();
-
-			if ($respCode >= 400) {
-				return ['error' => $this->l10n->t('Bad credentials')];
-			} else {
-				if ($jsonResponse) {
-					return json_decode($body, true);
-				} else {
-					return $body;
-				}
-			}
-		} catch (ServerException | ClientException $e) {
-			$body = $e->getResponse()->getBody();
-			$this->logger->error('Mattermost API error : ' . $body, ['app' => Application::APP_ID]);
-			return ['error' => $e->getMessage()];
-		} catch (Exception | Throwable $e) {
-			$this->logger->error('Mattermost API error', ['exception' => $e, 'app' => Application::APP_ID]);
-			return ['error' => $e->getMessage()];
-		}
+		return $this->networkService->request($userId, $mattermostUrl, $endPoint, $params, $method, $jsonResponse);
 	}
 
 	/**
